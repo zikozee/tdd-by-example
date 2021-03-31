ce29467b13022d3373bdcf212cfed675d82af15e
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-04-01 00:48:19 +0100

    prevented using direct instantiation of classes and delegated all to the Money Class

--- src/main/java/com/zikozee/Franc.java
+++ src/main/java/com/zikozee/Franc.java
@@ -10,7 +10,7 @@ public class Franc extends Money{
         this.amount = amount;
     }
 
-    Franc times(int multiplier){
+    public Money times(int multiplier){
         return new Franc(amount * multiplier);
     }
 
--- src/main/java/com/zikozee/Money.java
+++ src/main/java/com/zikozee/Money.java
@@ -4,9 +4,19 @@ package com.zikozee;
  * @author : zikoz
  * @created : 01 Apr, 2021
  */
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object){
         Money money =(Money) object;
