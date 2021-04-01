812aba0bbb22b9a523493b953f46612ad044daa1
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-04-01 01:02:41 +0100

    implemented currency only in money class

--- src/main/java/com/zikozee/Dollar.java
+++ src/main/java/com/zikozee/Dollar.java
@@ -6,12 +6,12 @@ package com.zikozee;
  */
 public class Dollar extends Money{
 
-    public Dollar(int amount) {
-        this.amount = amount;
+    public Dollar(int amount, String currency) {
+        super(amount, currency);
     }
 
     public Money times(int multiplier){
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 
 }
--- src/main/java/com/zikozee/Franc.java
+++ src/main/java/com/zikozee/Franc.java
@@ -6,12 +6,12 @@ package com.zikozee;
  */
 public class Franc extends Money{
 
-    public Franc(int amount) {
-        this.amount = amount;
+    public Franc(int amount, String currency) {
+        super(amount, currency);
     }
 
     public Money times(int multiplier){
-        return new Franc(amount * multiplier);
+        return Money.franc(amount * multiplier);
     }
 
 }
